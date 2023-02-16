8c818d7cf5c8b970be04e6d4d93c88eb869e2ab3
Evgeniy Putilin <mr.putilinz@gmail.com>
2023-02-16 18:19:28 +0300

    added feature/di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,10 +1,12 @@
 package ru.netology.repository;
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
+@Repository
 public class PostRepository {
   protected ConcurrentHashMap<Long, Post> postStorage = new ConcurrentHashMap<>();
   private final AtomicLong count = new AtomicLong(0);
